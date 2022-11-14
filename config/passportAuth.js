3bd9d0660c5836ad30cb0293ca523ae8144ab23d
Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>
2022-11-14 14:11:41 +0530

    Update
    Signed-off-by: Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>

--- config/passportAuth.js
+++ config/passportAuth.js
@@ -16,7 +16,8 @@ passport.use(
 		{
 			clientID: `${process.env.CLIENT}`,
 			clientSecret: `${process.env.CLIENT_SECRET}`,
-			callbackURL: `${process.env.CALLBACK_URL}`,
+			callbackURL: '/auth/google/callback',
+			scope: ['profile', 'email'],
 			passReqToCallback: true
 		},
 		async function (request, accessToken, refreshToken, profile, done) {
