392495edc47307430b027655e66924e035bad303
Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>
2022-11-23 10:13:08 +0530

    Navigation preload active
    Signed-off-by: Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>

--- Client/src/index.js
+++ Client/src/index.js
@@ -11,22 +11,25 @@ root.render(
 		<App />
 	</Provider>
 );
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker
-		.register('/service-worker.js')
-		.then((registration) => {
-			registration.onupdatefound = () => {
-				const newWorker = registration.installing;
 
-				newWorker.onstatechange = function () {
-					if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
-						if (window.confirm('New Version Available!Try Reload')) {
-							window.location.reload(true);
+window.addEventListener('load', () => {
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker
+			.register('/service-worker.js')
+			.then((registration) => {
+				registration.onupdatefound = () => {
+					const newWorker = registration.installing;
+
+					newWorker.onstatechange = function () {
+						if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+							if (window.confirm('New Version Available!Try Reload')) {
+								window.location.reload(true);
+							}
 						}
-					}
+					};
 				};
-			};
-			registration.update();
-		})
-		.catch((error) => console.error('Service worker not registered'));
-}
+				registration.update();
+			})
+			.catch((error) => console.error('Service worker not registered'));
+	}
+});
--- Client/src/service-worker.js
+++ Client/src/service-worker.js
@@ -1,16 +1,26 @@
-/* eslint-disable no-restricted-globals */ 
+/* eslint-disable no-restricted-globals */
 import { clientsClaim } from 'workbox-core';
 import { precacheAndRoute } from 'workbox-precaching';
+import * as navigationPreload from 'workbox-navigation-preload';
+import { NetworkOnly } from 'workbox-strategies';
+import { registerRoute, NavigationRoute } from 'workbox-routing';
 
 clientsClaim();
 precacheAndRoute(self.__WB_MANIFEST);
+navigationPreload.enable()
 
-self.addEventListener('install',()=>{
-  	self.skipWaiting();
-})
+const navigationRoute = new NavigationRoute(
+	new NetworkOnly({
+		cacheName: 'navigations'
+	})
+);
+self.addEventListener('install', () => {
+	self.skipWaiting();
+});
 
 self.addEventListener('message', (event) => {
 	if (event.data && event.data.type === 'SKIP_WAITING') {
 		self.skipWaiting();
 	}
 });
+registerRoute(navigationRoute);
